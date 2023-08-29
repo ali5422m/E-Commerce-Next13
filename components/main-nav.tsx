472c94bf0a37cbf215d94805ea9aa72a16687aa3
Ali Mohammadinia <alimohammadinia3141@gmail.com>
2023-08-29 16:54:26 +0330

    creating categories page

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -21,6 +21,11 @@ const MainNav = ({ className, ...props}: React.HTMLAttributes<HTMLElement>) => {
             active: pathname === `/${params.storeId}/billboards`
         },
         {
+            href: `/${params.storeId}/categories`,
+            label: 'Categories',
+            active: pathname === `/${params.storeId}/categories`
+        },
+        {
             href: `/${params.storeId}/settings`,
             label: 'Settings',
             active: pathname === `/${params.storeId}/settings`
